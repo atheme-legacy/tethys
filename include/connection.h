d6b7ec6fd65921442a8b4197e24f5b5a356256ae
Alex Iadicicco <alex@ajitek.net>
2013-03-04 10:55:53 -0700

    Add connection stuff

--- include/connection.h
+++ include/connection.h
@@ -1,6 +1,8 @@
 #ifndef __INC_CONNECTION_H__
 #define __INC_CONNECTION_H__
 
+#define U_CONNECTION_OBUFSIZE 2048
+
 struct u_connection {
 	struct u_io_fd *sock;
 	struct u_linebuf ibuf;
@@ -8,6 +10,15 @@ struct u_connection {
 	int obuflen, obufsize;
 };
 
-extern void u_connection_init(); /* u_connection*, int obufsize */
+struct u_conn_origin {
+	struct u_io_fd *sock;
+	void (*cb)(); /* u_io_fd* */
+};
+
+extern void u_connection_init(); /* u_connection* */
+extern void u_connection_obufsize(); /* u_connection*, int obufsize */
+
+/* u_io*, u_long addr, u_short port, void (*cb)(); */
+extern struct u_conn_origin *u_conn_origin_create();
 
 #endif
---
+++ src/connection.c
@@ -0,0 +1,100 @@
+#include "ircd.h"
+
+static void origin_recv();
+
+void u_connection_init(conn)
+struct u_connection *conn;
+{
+	conn->sock = NULL;
+	u_linebuf_init(&conn->ibuf);
+	conn->obuf = malloc(U_CONNECTION_OBUFSIZE);
+	conn->obuflen = 0;
+	conn->obufsize = U_CONNECTION_OBUFSIZE;
+}
+
+/* sadfaec */
+void u_connection_obufsize(conn, obufsize)
+struct u_connection *conn;
+int obufsize;
+{
+	char *buf;
+
+	if (conn->obufsize == obufsize)
+		return;
+
+	if (conn->obuflen > obufsize)
+		conn->obuflen = obufsize;
+
+	buf = malloc(obufsize);
+	memcpy(buf, conn->obuf, conn->obuflen);
+	free(conn->obuf);
+	conn->obuf = buf;
+	conn->obufsize = obufsize;
+}
+
+struct u_conn_origin *u_conn_origin_create(io, addr, port, cb)
+struct u_io *io;
+u_long addr;
+u_short port;
+void (*cb)();
+{
+	struct sockaddr_in sa;
+	struct u_conn_origin *orig;
+	int fd;
+
+	if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		goto out;
+
+	sa.sin_family = AF_INET;
+	sa.sin_port = port;
+	sa.sin_addr.s_addr = addr;
+
+	if (bind(fd, &sa, sizeof(sa)) < 0)
+		goto out;
+
+	if (listen(fd, 5) < 0)
+		goto out;
+
+	/* TODO: setsockopt */
+
+	if (!(orig = malloc(sizeof(*orig))))
+		goto out_close;
+
+	if (!(orig->sock = u_io_add_fd(io, fd)))
+		goto out_free;
+
+	orig->cb = cb;
+	orig->sock->recv = origin_recv;
+	orig->sock->send = NULL;
+	orig->sock->priv = orig;
+
+	return orig;
+
+out_free:
+	free(orig);
+out_close:
+	close(fd);
+out:
+	return NULL;
+}
+
+static void origin_recv(sock)
+struct u_io_fd *sock;
+{
+	struct u_conn_origin *orig = sock->priv;
+	struct u_io_fd *iofd;
+	struct sockaddr addr;
+	int addrlen;
+	int fd;
+
+	if ((fd = accept(orig->fd, &addr, &addrlen)) < 0)
+		return;
+
+	if (!(iofd = u_io_add_fd(sock->io, fd)))
+		return; /* XXX */
+
+	iofd->priv = NULL;
+	iofd->recv = NULL;
+	iofd->send = NULL;
+	orig->cb(iofd);
+}
