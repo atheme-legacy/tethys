48e3f6015e69b7788cb40b4c669d56c27fed2d37
Alex Iadicicco <alex@ajitek.net>
2013-03-08 11:56:30 -0700

    string.[hc] -> util.[hc]
    This prevents conflicts with the system-provided string.h

--- include/string.h
+++
@@ -1,6 +0,0 @@
-#ifndef __INC_STRING_H__
-#define __INC_STRING_H__
-
-extern void u_memmove();
-
-#endif
---
+++ include/util.h
@@ -0,0 +1,6 @@
+#ifndef __INC_UTIL_H__
+#define __INC_UTIL_H__
+
+extern void u_memmove();
+
+#endif
