c1175e1f8aeaea1679ccc10274f06e63c028cefa
Alex Iadicicco <alex@ajitek.net>
2013-04-19 03:05:46 -0700

    Stylistic changes to m_userhost

--- src/c_user.c
+++ src/c_user.c
@@ -327,35 +327,34 @@ static void m_whois(conn, msg) u_conn *conn; u_msg *msg;
 static void m_userhost(conn, msg) u_conn *conn; u_msg *msg;
 {
 	/* USERHOST user1 user2... usern 
-	 * :host.irc 302 :user1=+~user@host user2=+~user@host ...
+	 * :host.irc 302 nick :user1=+~user@host user2=+~user@host ...
+	 * *        *****    **   = 8
 	 */
 	u_user *tu, *u = conn->priv;
-	int para;
-	int w;
-	int rem = 510;
-	char buf[512];
+	int i, w, rem = 510;
+	char buf[512], data[512];
 	char *ptr = buf;
 
 	rem -= strlen(me.name) + strlen(u->nick) + 8;
 
 	/* TODO - last param could contain multiple targets */
-	for (para = 0; para != msg->argc || rem <= 0; para++)
-	{
-		tu = u_user_by_nick(msg->argv[para]);
+	for (i=0; i<msg->argc; i++) {
+		tu = u_user_by_nick(msg->argv[i]);
 		if (tu == NULL)
 			continue;
 
-		w = strlen(tu->nick) + strlen(tu->ident) + strlen(tu->host);
-		w += (tu->flags & UMODE_OPER) ? 4 : 3;
-
-		if ((rem - w) <= 0)
-			/* TODO - overflow handling */
+		w = sprintf(data, "%s%s=%c%s@%s", tu->nick,
+		            ((tu->flags & UMODE_OPER) ? "*" : ""),
+		            (tu->away[0] ? '-' : '+'),
+		            tu->ident, tu->host);
+		if (w + 1 > rem)
 			break;
 
-		ptr += sprintf(ptr, "%s%s=%c%s@%s ", tu->nick,
-				((tu->flags & UMODE_OPER) ? "*" : ""),
-				(tu->away[0] ? '-' : '+'),
-				tu->ident, tu->host);
+		if (ptr != buf)
+			*ptr++ = ' ';
+
+		u_strlcpy(ptr, data, rem);
+		ptr += w;
 		rem -= w;
 	}
 
