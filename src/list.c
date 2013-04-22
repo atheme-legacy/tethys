1a76a96ae1fb908a36d21078d1d7e865bfb49acc
Alex Iadicicco <alex@ajitek.net>
2013-04-21 22:49:15 -0700

    Un-dumb list.c

--- src/list.c
+++ src/list.c
@@ -6,93 +6,64 @@
 
 #include "ircd.h"
 
-#define BEGIN {
-#define END }
-#define IF if(
-#define THEN ){
-#define EQ ==
-#define YIELD return
-#define RETURN return
-#define FAILURE return NULL
-#define NODE u_list
-#define PTR *
-#define REF void*
-#define FIELD ->
-#define ASSIGN =
-#define VOID void
-#define INTEGER int
-#define NEW malloc(sizeof(
-#define RELEASE free((
-#define CELL ))
+void u_list_init(list) u_list *list;
+{
+	list->data = (void *)0;
+	list->prev = list;
+	list->next = list;
+}
 
-VOID u_list_init(list)
-NODE PTR list;
-BEGIN
-	list FIELD data ASSIGN (REF)0;
-	list FIELD prev ASSIGN list;
-	list FIELD next ASSIGN list;
-END
+u_list *u_list_add(list, data) u_list *list; void *data;
+{
+	u_list *n;
 
-NODE PTR u_list_add(list, data)
-NODE PTR list;
-REF data;
-BEGIN
-	NODE PTR n;
+	n = malloc(sizeof(u_list));
+	if (n == NULL)
+		return NULL;
 
-	n ASSIGN NEW NODE CELL;
-	IF n EQ NULL
-	THEN
-		FAILURE;
-	END
+	n->data = data;
+	n->next = list;
+	n->prev = list->prev;
+	n->next->prev = n;
+	n->prev->next = n;
 
-	n FIELD data            ASSIGN data;
-	n FIELD next            ASSIGN list;
-	n FIELD prev            ASSIGN list FIELD prev;
-	n FIELD next FIELD prev ASSIGN n;
-	n FIELD prev FIELD next ASSIGN n;
+	list->data = (void*)((int)(list->data) + 1);
 
-	list FIELD data         ASSIGN (REF)((INTEGER)(list FIELD data) + 1);
+	return n;
+}
 
-	YIELD n;
-END
+u_list *u_list_contains(list, data) u_list *list; void *data;
+{
+	u_list *n;
 
-NODE PTR u_list_contains(list, data)
-NODE PTR list;
-REF data;
-BEGIN
-	NODE PTR n;
+	U_LIST_EACH(n, list) {
+		if (n->data == data)
+			return n;
+	}
 
-	U_LIST_EACH(n, list)
-	BEGIN
-		IF n FIELD data EQ data
-		THEN
-			YIELD n;
-		END
-	END
+	return NULL;
+}
 
-	FAILURE;
-END
+int u_list_is_empty(list) u_list *list;
+{
+	return list->next == list;
+} 
 
-INTEGER u_list_is_empty(list)
-NODE PTR list;
-BEGIN
-	YIELD list FIELD next EQ list;
-END 
+int u_list_size(list) u_list *list;
+{
+	return (int)(list->data);
+}
 
-INTEGER u_list_size(list)
-NODE PTR list;
-BEGIN
-	YIELD (INTEGER)(list FIELD data);
-END
+void *u_list_del_n(list, n) u_list *list, *n;
+{
+	void *data;
 
-REF u_list_del_n(list, n)
-NODE PTR list, PTR n;
-BEGIN
-	REF data;
-	n FIELD next FIELD prev ASSIGN n FIELD prev;
-	n FIELD prev FIELD next ASSIGN n FIELD next;
-	data ASSIGN n FIELD data;
-	RELEASE n CELL;
-	list FIELD data ASSIGN (REF)((INTEGER)(list FIELD data) - 1);
-	YIELD data;
-END
+	n->next->prev = n->prev;
+	n->prev->next = n->next;
+	data = n->data;
+
+	free(n);
+	list->data = (void*)((int)(list->data) - 1);
+
+	return data;
+}
