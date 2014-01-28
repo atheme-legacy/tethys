4fe77d294adef3222009d41f7d6cf7662e53c989
Alex Iadicicco <alex@ajitek.net>
2014-01-28 13:36:17 -0700

    Add EUID

---
+++ modules/core/euid.c
@@ -0,0 +1,47 @@
+/* Tethys, core/euid -- EUID command
+   Copyright (C) 2013 Alex Iadicicco
+
+   This file is protected under the terms contained
+   in the COPYING file in the project root */
+
+#include "ircd.h"
+
+static int c_s_euid(u_sourceinfo *si, u_msg *msg)
+{
+	u_user *u;
+
+	if (!si->s) {
+		u_log(LG_SEVERE, "EUID from unknown server %s", msg->srcstr);
+		return 0;
+	}
+
+	u = u_user_create_remote(si->s, msg->argv[7]);
+
+	u_user_set_nick(u, msg->argv[0], atoi(msg->argv[2]));
+	/* TODO: user modes */
+	u_strlcpy(u->ident, msg->argv[4], MAXIDENT+1);
+	u_strlcpy(u->host, msg->argv[5], MAXHOST+1);
+	u_strlcpy(u->ip, msg->argv[6], INET_ADDRSTRLEN);
+	u_strlcpy(u->gecos, msg->argv[msg->argc - 1], MAXGECOS+1);
+	u_strlcpy(u->realhost, msg->argv[8], MAXHOST+1);
+	if (msg->argv[9][0] != '*')
+		u_strlcpy(u->acct, msg->argv[9], MAXACCOUNT+1);
+
+	msg->propagate = CMD_DO_BROADCAST;
+
+	return 0;
+}
+
+static u_cmd euid_cmdtab[] = {
+	{ "EUID", SRC_SERVER, c_s_euid, 11, CMD_PROP_BROADCAST },
+	{ }
+};
+
+static int init_euid(u_module *m)
+{
+	u_cmds_reg(euid_cmdtab);
+}
+
+TETHYS_MODULE_V1(
+	"core/euid", "Alex Iadicicco", "EUID command",
+	init_euid, NULL);
