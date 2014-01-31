56424b91df1279816111da10fc339681c11b8bf9
Alex Iadicicco <alex@ajitek.net>
2014-01-31 15:54:52 -0700

    core/euid: Process user mode parameter

--- modules/core/oper.c
+++ modules/core/oper.c
@@ -18,6 +18,8 @@ static int c_lu_oper(u_sourceinfo *si, u_msg *msg)
 	u_conn_f(si->source, ":%U MODE %U :+o", si->u, si->u);
 	u_sendto_servers(NULL, ":%U MODE %U :+o", si->u, si->u);
 	u_user_num(si->u, RPL_YOUREOPER);
+	u_log(LG_VERBOSE, "%U now has user mode %s",
+	      si->u, u_user_modes(si->u));
 
 	return 0;
 }
