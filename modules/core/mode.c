a6877d5a4236a8285b013d6d5e4cc12b26206955
Alex Iadicicco <alex@ajitek.net>
2014-02-04 14:44:31 -0700

    core/mode: Kind of fix /MODE for channels

--- modules/core/mode.c
+++ modules/core/mode.c
@@ -6,9 +6,9 @@
 
 #include "ircd.h"
 
-/*
 static int mode_user(u_sourceinfo *si, char *s)
 {
+/*
 	u_modes m;
 
 	if (!s || !*s) {
@@ -36,38 +36,60 @@ static int mode_user(u_sourceinfo *si, char *s)
 	      si->u, u_user_modes(si->u));
 
 	return 0;
-}
-
-static void send_chan_mode_change(u_sourceinfo *si, u_modes *m, u_chan *c)
-{
-	if (m->u.buf[0] || m->u.data[0]) {
-		u_sendto_chan(c, NULL, ST_USERS, ":%I MODE %C %s%s",
-		              si, c, m->u.buf, m->u.data);
-	}
-
-	if (m->s.buf[0] || m->s.data[0]) {
-		u_sendto_servers(si->source, ":%I TMODE %u %C %s%s",
-		                 si, c->ts, c, m->s.buf, m->s.data);
-	}
-}
 */
+}
+
+/* TODO: this is kind of brain-damaged and needs to be redone to use buffers */
+
+static void cmode_stacker_put_external(u_modes *m, int on, char *param)
+{
+	u_sendto_chan(m->target, NULL, ST_USERS, ":%I MODE %c%c%s%s",
+	              m->setter, on ? '+' : '-',
+	              m->info->ch, param ? " " : "", param ? param : "");
+}
+
+static void cmode_stacker_put_status(u_modes *m, int on, void *tgt)
+{
+	u_chanuser *cu = tgt;
+	u_sendto_chan(m->target, NULL, ST_USERS, ":%I MODE %c%c %U",
+	              m->setter, on ? '+' : '-', m->info->ch, cu->u);
+}
+
+static void cmode_stacker_put_flag(u_modes *m, int on)
+{
+	u_sendto_chan(m->target, NULL, ST_USERS, ":%I MODE %c%c",
+	              m->setter, on ? '+' : '-', m->info->ch);
+}
+
+static void cmode_stacker_put_banlist(u_modes *m, int on, u_chanban *ban)
+{
+	u_sendto_chan(m->target, NULL, ST_USERS, ":%I MODE %c%c %s",
+	              m->setter, on ? '+' : '-', m->info->ch, ban->mask);
+}
+
+static u_mode_stacker cmode_stacker = {
+	.start          = NULL,
+	.put_external   = cmode_stacker_put_external,
+	.put_status     = cmode_stacker_put_status,
+	.put_flag       = cmode_stacker_put_flag,
+	.put_banlist    = cmode_stacker_put_banlist,
+	.end            = NULL,
+};
 
 /* this function is carefully written to handle both local and remote users
    and servers. */
 static int c_a_mode(u_sourceinfo *si, u_msg *msg)
 {
-/*
 	char *target = msg->argv[0];
 	int parc;
 	char **parv;
 	u_chan *c;
 	u_modes m;
-	u_mode_info *info;
 
 	if (!strchr(CHANTYPES, *target)) {
 		u_user *tu = u_user_by_ref(si->source, target);
 		if (tu == NULL) {
-			* legacy chary behavior *
+			/* legacy chary behavior */
 			u_user_num(si->u, ERR_NOSUCHCHANNEL, target);
 		} else if (si->u != tu) {
 			u_user_num(si->u, ERR_USERSDONTMATCH);
@@ -80,15 +102,21 @@ static int c_a_mode(u_sourceinfo *si, u_msg *msg)
 	if (!(c = u_chan_get(target)))
 		return u_user_num(si->u, ERR_NOSUCHCHANNEL, target);
 
+	m.access = NULL;
+
 	if (SRC_IS_LOCAL_USER(si)) {
-		m.perms = u_chan_user_find(c, si->u);
+		u_chanuser *cu = u_chan_user_find(c, si->u);
 
 		if (msg->argc == 1) {
 			return u_user_num(si->u, RPL_CHANNELMODEIS, c,
-					  u_chan_modes(c, !!m.perms));
+					  u_chan_modes(c, !!cu));
 		}
-	} else { * source is local server or remote user/server *
-		m.perms = &me;
+
+		if (cu->flags & CU_PFX_OP)
+			m.access = cu;
+
+	} else { /* source is local server or remote user/server */
+		m.access = &me;
 
 		if (msg->argc == 1)
 			return 0;
@@ -99,28 +127,21 @@ static int c_a_mode(u_sourceinfo *si, u_msg *msg)
 	if (parc > 5)
 		parc = 5;
 
+	m.ctx = &cmodes;
+	m.stacker = &cmode_stacker;
 	m.setter = si;
 	m.target = c;
-	m.flags = 0;
 
-	u_mode_process(&m, cmodes, parc, parv);
-
-	send_chan_mode_change(si, &m, c);
+	u_mode_process(&m, parc, parv);
 
 	if (SRC_IS_LOCAL_USER(si)) {
-		for (info=cmodes; info->ch; info++) {
-			if (!strchr(m.list, info->ch))
-				continue;
-
-			u_chan_send_list(c, si->u, memberp(c, info->data));
-		}
-
-		if (m.flags & CM_DENY)
-			u_user_num(si->u, ERR_CHANOPRIVSNEEDED, c);
+		if (m.errors & MODE_ERR_NO_ACCESS)
+			u_src_num(si, ERR_CHANOPRIVSNEEDED, c);
+		if (m.errors & MODE_ERR_NOT_OPER)
+			u_src_num(si, ERR_NOPRIVILEGES);
 	}
 
 	return 0;
-*/
 }
 
 static int c_r_tmode(u_sourceinfo *si, u_msg *msg)
