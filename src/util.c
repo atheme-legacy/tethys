atheme-legacy/tethys

commit 8088c05c5071fd613310422333edbc1f3bbcb712

IPv6 compatibility for Tethys
Tethys does not work with IPv6 at the moment because it is hardcoded to
bind IPv4 sockets, and some of its networking code assumes all addresses
will be IPv4 addresses.

This patch introduces compatibility for IPv6 clients, however, problems
experienced with reverse DNS resolution of IPv6 client addresses means
that, for the moment, resolution is not performed on such clients.

Specifically, this patch fixes the following bugs:

* Populating in_addr structures manually instead of using getaddrinfo
* Using INET_ADDRSTRLEN in some places instead of INET6_ADDRSTRLEN
* Hardcoding AF_INET in some places without an AF_INET6 provision too
* CIDR matching & conversion code not supporting IPv6 addresses/networks
* Using the wrong variable type for network ports
* Using the wrong address family in some comparisons
* Not properly checking the return value of inet_pton()

src/util.c
@@ -6,51 +6,107 @@
 
 #include "ircd.h"
 
-void u_cidr_to_str(u_cidr *cidr, char *s)
+char* u_cidr_to_str(u_cidr *cidr, char *dst)
 {
-	struct in_addr in;
+	char *out = dst;
+	memset(out, 0, CIDR_ADDRSTRLEN);
+	if (! u_ntop((struct sockaddr*) &(cidr->addr), out))
+		return NULL;
 
-	in.s_addr = htonl(cidr->addr);
-	u_ntop(&in, s);
-	s += strlen(s);
-	sprintf(s, "/%d", cidr->netsize);
+	out += strlen(out);
+	sprintf(out, "/%hu", (ushort) cidr->netsize);
+	return dst;
 }
 
-void u_str_to_cidr(char *s, u_cidr *cidr)
+u_cidr* u_str_to_cidr(char *src, u_cidr *cidr)
 {
-	struct in_addr in;
-	char *p;
+	uint8_t max_netsize;
+	char *p, *v4, *v6;
+	int netsize;
 
-	p = strchr(s, '/');
+	memset(cidr, 0, sizeof(*cidr));
+
+	v4 = strchr(src, '.');
+	v6 = strchr(src, ':');
+	if ((! v4 && ! v6) || (v4 && v6))
+		return NULL;
+
+	if (v4 && ! v6)
+		max_netsize = 32;
+
+	if (! v4 && v6)
+		max_netsize = 128;
+
+	p = strchr(src, '/');
 	if (p == NULL) {
-		cidr->netsize = 32;
+		cidr->netsize = max_netsize;
 	} else {
 		*p++ = '\0';
-		cidr->netsize = atoi(p);
-		if (cidr->netsize < 0)
-			cidr->netsize = 0;
-		else if (cidr->netsize > 32)
-			cidr->netsize = 32;
+		netsize = atoi(p);
+
+		if (netsize < 0)
+			netsize = 0;
+
+		if (netsize > max_netsize)
+			netsize = max_netsize;
+
+		cidr->netsize = (uint8_t) netsize;
 	}
 
-	u_aton(s, &in);
-	cidr->addr = ntohl(in.s_addr);
+	if (! u_pton(src, (struct sockaddr*) &(cidr->addr), NULL))
+		return NULL;
+
+	return cidr;
 }
 
 int u_cidr_match(u_cidr *cidr, char *s)
 {
-	struct in_addr in;
-	ulong mask, addr;
-
+	/* A netmask of zero will match anything */
 	if (cidr->netsize == 0)
-		return 1; /* everything is in /0 */
-	/* 8 becomes 0x00ffffff, 21 becomes 0x000007ff, etc */
-	mask = (1 << (32 - cidr->netsize)) - 1;
+		return 1;
 
-	u_aton(s, &in);
-	addr = ntohl(in.s_addr);
+	/* Copy the 2 addresses into byte arrays. These are guaranteed
+	 * to be in network byte order (big endian), since they are only
+	 * ever set with inet_pton(), which saves them in network byte
+	 * order for transmission on-wire. You can therefore compare
+	 * them byte-by-byte in order.
+	 */
+	uint8_t addr_cidr[16], addr_given[16];
+	switch (cidr->addr.ss_family) {
+		case AF_INET:
+			memcpy(addr_cidr, &(((struct sockaddr_in*) &(cidr->addr))->sin_addr), 4);
+			inet_pton(AF_INET, s, (struct in_addr*) addr_given);
+			break;
+		case AF_INET6:
+			memcpy(addr_cidr, &(((struct sockaddr_in6*) &(cidr->addr))->sin6_addr), 16);
+			inet_pton(AF_INET6, s, (struct in6_addr*) addr_given);
+			break;
+		default:
+			return 0;
+	}
 
-	return (addr | mask) == (cidr->addr | mask);
+	/* A network mask in decimal CIDR form (e.g. /24) is just a nice
+	 * way of representing what a network mask actually is: a sequence
+	 * of bits that all addresses within that network start with (when
+	 * AND-masked). e.g. in a /28 network, all hosts within that network
+	 * start with the same 28 bits.
+	 *
+	 * Therefore, we only need to test the first netmask/8 bytes of
+	 * the address, and if there are any bits left after that division,
+	 * those too.
+	 */
+	uint8_t octs = cidr->netsize / 8;
+	uint8_t bits = cidr->netsize % 8;
+	if (memcmp(addr_cidr, addr_given, octs) == 0) {
+		if (bits == 0)
+			return 1;
+
+		/* You could do -1 << x but it triggers warnings in uints */
+		bits = (1 << (8 - bits)) - 1;
+		if ((addr_cidr[octs] & bits) == (addr_given[octs] & bits))
+			return 1;
+	}
+	return 0;
 }
 
 void u_bitmask_reset(u_bitmask_set *bm)
@@ -195,34 +251,46 @@ int irccmp(char *s1, char *s2)
 	return mapcmp(s1, s2, rfc1459_casemap);
 }
 
-void u_ntop(struct in_addr *in, char *s)
+char* u_ntop(struct sockaddr *sa, char *dst)
 {
-	u_strlcpy(s, inet_ntoa(*in), INET_ADDRSTRLEN);
+	switch (sa->sa_family) {
+		case AF_INET:
+			inet_ntop(AF_INET, &((struct sockaddr_in*) sa)->sin_addr, dst, INET_ADDRSTRLEN);
+			return dst;
+		case AF_INET6:
+			inet_ntop(AF_INET6, &((struct sockaddr_in6*) sa)->sin6_addr, dst, INET6_ADDRSTRLEN);
+			if (dst[0] == ':') {
+				// IP addresses starting with : screws with the IRC packet format. Prepend a 0
+				dst[0] = '0';
+				inet_ntop(AF_INET6, &((struct sockaddr_in6*) sa)->sin6_addr, dst + 1, INET6_ADDRSTRLEN);
+			}
+			return dst;
+	}
+	return NULL;
 }
 
-void u_aton(char *s, struct in_addr *in)
+struct sockaddr* u_pton(const char *src, struct sockaddr *sa, socklen_t *salen)
 {
-	in->s_addr = inet_addr(s);
-}
-
-void u_pton(const char *src, struct sockaddr_storage *ss)
-{
-	void *v4, *v6;
-
-	memset(ss, 0, sizeof(*ss));
-
-	v4 = &((struct sockaddr_in*) ss)->sin_addr;
-	v6 = &((struct sockaddr_in6*)ss)->sin6_addr;
-
-	if (!(inet_pton(AF_INET, src, v4) < 0)) {
-		ss->ss_family = AF_INET;
-		return;
+	if (salen != NULL) {
+		memset(sa, 0, *salen);
+	} else {
+		memset(sa, 0, sizeof(struct sockaddr));
 	}
+	if (strchr(src, '.') && inet_pton(AF_INET, src, &((struct sockaddr_in*) sa)->sin_addr) == 1) {
+		if (salen != NULL)
+			*salen = sizeof(struct sockaddr_in);
 
-	if (!(inet_pton(AF_INET6, src, v6) < 0)) {
-		ss->ss_family = AF_INET6;
-		return;
+		sa->sa_family = AF_INET;
+		return sa;
 	}
+	if (strchr(src, ':') && inet_pton(AF_INET6, src, &((struct sockaddr_in6*) sa)->sin6_addr) == 1) {
+		if (salen != NULL)
+			*salen = sizeof(struct sockaddr_in6);
+
+		sa->sa_family = AF_INET6;
+		return sa;
+	}
+	return NULL;
 }
 
 char *cut(char **p, char *delim)

include/auth.h
@@ -53,7 +53,7 @@ struct u_oper_block {
 
 struct u_link_block {
 	char name[MAXSERVERNAME+1];
-	char host[INET_ADDRSTRLEN];
+	char host[INET6_ADDRSTRLEN];
 	int port;
 	char recvpass[MAXPASSWORD+1];
 	char sendpass[MAXPASSWORD+1];

include/ircd.h
@@ -42,6 +42,10 @@
 #define INET_ADDRSTRLEN 16
 #endif
 
+#ifndef INET6_ADDRSTRLEN
+#define INET6_ADDRSTRLEN 46
+#endif
+
 /* size of string buffers allocated on the stack */
 #define BUFSIZE 4096
 
@@ -105,6 +109,6 @@ extern char startedstr[256];
 extern char *crypt(const char*, const char*);
 
 extern char *main_argv0;
-extern short opt_port;
+extern ushort opt_port;
 
 #endif

include/link.h
@@ -72,7 +72,7 @@ extern void u_link_vnum(u_link *link, const char *tgt, int num, va_list va);
 extern int u_link_num(u_link *link, int num, ...);
 extern void u_link_flush_input(u_link *link);
 
-extern u_link_origin *u_link_origin_create(mowgli_eventloop_t*, short);
+extern int u_link_origin_create(mowgli_eventloop_t*, ushort);
 
 extern int init_link(void);
 

include/user.h
@@ -56,7 +56,7 @@ struct u_user {
 
 	char ident[MAXIDENT+1];
 
-	char ip[INET_ADDRSTRLEN];
+	char ip[INET6_ADDRSTRLEN];
 	char realhost[MAXHOST+1];
 	char host[MAXHOST+1];
 

include/util.h
@@ -7,17 +7,15 @@
 #ifndef __INC_UTIL_H__
 #define __INC_UTIL_H__
 
-#define CIDR_ADDRSTRLEN (INET_ADDRSTRLEN+3)
+#define CIDR_ADDRSTRLEN (INET6_ADDRSTRLEN+4)
 
-typedef struct u_cidr u_cidr;
+typedef struct u_cidr {
+	struct sockaddr_storage addr;
+	uint8_t netsize;
+} u_cidr;
 
-struct u_cidr {
-	ulong addr;
-	int netsize;
-};
-
-extern void u_cidr_to_str(u_cidr*, char*);
-extern void u_str_to_cidr(char*, u_cidr*);
+extern char* u_cidr_to_str(u_cidr*, char*);
+extern u_cidr* u_str_to_cidr(char*, u_cidr*);
 extern int u_cidr_match(u_cidr*, char*);
 
 typedef unsigned long u_bitmask_set;
@@ -43,10 +41,8 @@ extern int irccmp(char *s1, char *s2);
 
 #define u_strlcpy mowgli_strlcpy
 #define u_strlcat mowgli_strlcat
-extern void u_ntop(struct in_addr*, char*);
-extern void u_aton(char*, struct in_addr*);
-
-extern void u_pton(const char*, struct sockaddr_storage *ss);
+extern char* u_ntop(struct sockaddr*, char*);
+extern struct sockaddr* u_pton(const char*, struct sockaddr*, socklen_t*);
 
 extern char *cut(char **p, char *delim);
 

modules/core/connect.c
@@ -25,47 +25,39 @@ static void notice(u_user *u, const char *fmt, ...)
 
 static int c_lo_connect(u_sourceinfo *si, u_msg *msg)
 {
-	u_link_block *block;
-	int port;
-	int af;
-	struct sockaddr_storage addr;
-	socklen_t addrlen = sizeof(addr);
-
 	/* TODO: remote version */
 
-	if (!(block = u_find_link(msg->argv[0]))) {
+	u_link_block *block;
+	if (! (block = u_find_link(msg->argv[0]))) {
 		notice(si->u, "connect: host %s not in config", msg->argv[0]);
 		return 0;
 	}
 
-	port = block->port;
+	int port = block->port;
 	if (msg->argc > 1) {
 		port = atoi(msg->argv[1]);
-		if (port <= 0 || port >= 65536) {
+		if (port < 1 || port > 65535) {
 			port = block->port;
 			notice(si->u, "connect: %s: invalid port number; "
 			       "using %d in config", msg->argv[1], port);
 		}
 	}
 
-	u_pton(block->host, &addr);
-
-	switch (addr.ss_family) {
-	case AF_INET6:
-		((struct sockaddr_in*) &addr)->sin_port  = htons(port);
-		break;
-
-	case AF_INET:
-		((struct sockaddr_in6*)&addr)->sin6_port = htons(port);
-		break;
-
-	default:
-		notice(si->u, "connect: could not make host %s in config "
-		       "into an address", block->host);
+	struct sockaddr_storage addr;
+	socklen_t addrlen = sizeof(addr);
+	if (! u_pton(block->host, (struct sockaddr*) &addr, &addrlen)) {
+		notice(si->u, "connect: couldn't make host %s into an address", block->host);
 		return 0;
 	}
-
-	u_link_connect(base_ev, block, (struct sockaddr*)&addr, addrlen);
+	switch (addr.ss_family) {
+		case AF_INET:
+			((struct sockaddr_in*) &addr)->sin_port = htons((ushort) port);
+			break;
+		case AF_INET6:
+			((struct sockaddr_in6*) &addr)->sin6_port = htons((ushort) port);
+			break;
+	}
+	u_link_connect(base_ev, block, (struct sockaddr*) &addr, addrlen);
 
 	return 0;
 }

modules/core/euid.c
@@ -27,7 +27,7 @@ static int c_s_euid(u_sourceinfo *si, u_msg *msg)
 	u_user_set_nick(u, msg->argv[0], atoi(msg->argv[2]));
 	u_strlcpy(u->ident, msg->argv[4], MAXIDENT+1);
 	u_strlcpy(u->host, msg->argv[5], MAXHOST+1);
-	u_strlcpy(u->ip, msg->argv[6], INET_ADDRSTRLEN);
+	u_strlcpy(u->ip, msg->argv[6], INET6_ADDRSTRLEN);
 	u_strlcpy(u->gecos, msg->argv[msg->argc - 1], MAXGECOS+1);
 	u_strlcpy(u->realhost, msg->argv[8], MAXHOST+1);
 	if (msg->argv[9][0] != '*')

commit 2456a24764729620735010db1a37fb6fb060c1a3

Fix regression introduced by commit 8088c05c50
Regression:
CIDR matching code now returns an incorrect result when dealing
with netmasks that are not a multiple of 8 bits.

Bug:
'-1 << x' is not the same as '(1 << x) - 1', but rather the
inverse of the latter.

Fix:
Invert the result.

Reported-by: shadowninja@minetest.net

src/util.c
@@ -102,7 +102,7 @@ int u_cidr_match(u_cidr *cidr, char *s)
 			return 1;
 
 		/* You could do -1 << x but it triggers warnings in uints */
-		bits = (1 << (8 - bits)) - 1;
+		bits = ~((1 << (8 - bits)) - 1);
 		if ((addr_cidr[octs] & bits) == (addr_given[octs] & bits))
 			return 1;
 	}